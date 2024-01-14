a810da757df9966fa97d7e475dfafa7eae77640d
MaxAve <maxave909@gmail.com>
2024-01-14 13:56:53 +0100

    Remove a bunch of crap

--- launch.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    std::cout<<"[Sudoku] Starting...\n";
-    system("cd src && javac Sudoku.java && cd ../bin && java Sudoku");
-    return 0;
-}
